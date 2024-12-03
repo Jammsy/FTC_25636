38fee8652e002ce839187373a16a21ef042aa3ed
Jammsy <90413617+Jammsy@users.noreply.github.com>
2024-12-03 08:11:42 -0600

    Logic for pivot and intake, added linear slide motors. Can you double check the if statements?

--- mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compModeOne.java
+++ mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compModeOne.java
@@ -1,42 +1,17 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
 package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 
 /*
@@ -83,10 +58,12 @@ public class compModeOne extends LinearOpMode {
     private DcMotor pivotTwo = null;
     private DcMotor linSlideLeft = null;
     private DcMotor linSlideRight = null;
+    private Servo intakeServo = null;
 
     @Override
     public void runOpMode() {
 
+        //Gamepad gamepad1 = new Gamepad();
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFrontDrive");
@@ -100,6 +77,7 @@ public class compModeOne extends LinearOpMode {
         linSlideLeft = hardwareMap.get(DcMotor.class, "linSlideLeft");
         linSlideRight = hardwareMap.get(DcMotor.class, "linSlideRight");
 
+        intakeServo = hardwareMap.get(Servo.class, "intakeServo");
 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -136,12 +114,14 @@ public class compModeOne extends LinearOpMode {
         while (opModeIsActive()) {
             double max;
             double pivot;
+            double slide;
+            double intake;
 
 
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  -gamepad1.right_stick_x;
+            double axial   = gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral =  -gamepad1.left_stick_x;
+            double yaw     = -gamepad1.right_stick_x;//gamepad1.right_stick_x;
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
@@ -171,26 +151,45 @@ public class compModeOne extends LinearOpMode {
             leftBackDrive.setPower(leftBackPower);
             rightBackDrive.setPower(rightBackPower);
 
-
-            if(gamepad1.a == true){
+            //pivot logic
+            if(gamepad1.right_trigger > 0.3){
                 pivot = 1;
+            }else if(gamepad1.left_trigger > 0.3) {
+                pivot = -1;
             }else{
                 pivot = 0;
             }
 
+            //linear slide logic
+            if(gamepad1.dpad_up){
+                slide = 1;
+            }else if(gamepad1.dpad_down){
+                slide = -1;
+            }else{
+                slide = 0;
+            }
 
+            //intake logic
+            if(gamepad1.a){
+                intake = 45;
+            }else{
+                intake = 0;
+            }
 
-            // Show the elapsed game time and wheel power.
+            //Set Power To motors as well as reset defaults
             pivotOne.setPower(pivot);
             pivotTwo.setPower(pivot);
+            linSlideRight.setPower(slide);
+            linSlideLeft.setPower(slide);
+            intakeServo.setPosition(intake);
 
-/*
+            //Show status, DT wheel power, and slide / intake debug
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Pivot one / two ", "%4,2f, %4.2f", pivotOne.getPower(), pivotTwo.getPower());
+            telemetry.addData("Intake Position", "%3f", intakeServo.getPosition());
             telemetry.update();
-*/
+
         }
     }}
 
