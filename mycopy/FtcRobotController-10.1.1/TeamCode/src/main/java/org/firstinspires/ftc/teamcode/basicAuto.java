b1304e889079539af8458ba9dbfbd8077d29a55f
Jammsy <90413617+Jammsy@users.noreply.github.com>
2024-12-05 12:05:14 -0600

    Field Oriented drivetrain, fixed button logic / intake, added basic test auto

---
+++ mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicAuto.java
@@ -0,0 +1,157 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@Autonomous(name = "basicAuto")
+//@Disabled
+public class basicAuto extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor pivotOne = null;
+    private DcMotor pivotTwo = null;
+    private Servo intakeServo = null;
+
+    @Override
+    public void runOpMode() {
+        //Hardware map
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+        pivotOne = hardwareMap.get(DcMotor.class, "pivotOne");
+        pivotTwo = hardwareMap.get(DcMotor.class, "pivotTwo");
+        intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+
+        //Motor Direction Set
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        pivotOne.setDirection(DcMotor.Direction.REVERSE);
+        pivotTwo.setDirection(DcMotor.Direction.FORWARD);
+        //Breaking modes for motors
+        /*leftFrontDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        leftBackDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        rightFrontDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        rightBackDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        rightFrontDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));*/
+        pivotOne.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        pivotTwo.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            int forward = 1000;
+            int backward = -1000;
+            int armPos = 0;
+
+            //Arm Pivot Encoder Stuff
+            /*
+            double CPR1 = 336;
+            double pivot1Encoder = pivotOne.getCurrentPosition();
+            double pivot2Encoder = pivotTwo.getCurrentPosition();
+
+            double p1Rev = pivot1Encoder / CPR1;
+            double p2Rev = pivot2Encoder / CPR1;
+
+            double p1Angle = p1Rev * 360;
+            double p1Ang = p1Angle % 360;
+            double p2Angle = p2Rev * 360;
+            double p2Ang = p2Angle % 360;
+*/
+            //Drivetrain Encoder stuff
+            /*
+            double CPR = 252;
+            double frEncoder = rightFrontDrive.getCurrentPosition();
+            double flEncoder = leftFrontDrive.getCurrentPosition();
+            double brEncoder = rightBackDrive.getCurrentPosition();
+            double blEncoder = leftBackDrive.getCurrentPosition();
+
+            double frRev = frEncoder/CPR;
+            double flRev = flEncoder/CPR;
+            double brRev = brEncoder/CPR;
+            double blRev = blEncoder/CPR;
+
+            double angle1 = frRev * 360;
+            double frAng = angle1 % 360;
+            double angle2 = flRev * 360;
+            double flAng = angle2 % 360;
+            double angle3 = brRev * 360;
+            double brAng = angle3 % 360;
+            double angle4 = blRev * 360;
+            double blAng = angle4 % 360;
+*/
+
+            rightFrontDrive.setTargetPosition(forward);
+            leftFrontDrive.setTargetPosition(forward);
+            rightBackDrive.setTargetPosition(-(forward));
+            rightBackDrive.setTargetPosition(-(forward));
+            pivotOne.setTargetPosition(armPos);
+            pivotTwo.setTargetPosition(armPos);
+
+            rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            pivotOne.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            pivotTwo.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            rightFrontDrive.setPower(0.5);
+            leftFrontDrive.setPower(0.5);
+            rightBackDrive.setPower(0.5);
+            rightBackDrive.setPower(0.5);
+            pivotOne.setPower(0.6);
+            pivotTwo.setPower(0.6);
+            //intakeServo.setPosition(0.75);
+
+
+            //Show status, DT wheel power, and slide / intake debug
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Intake Position", "%2f", intakeServo.getPosition());
+            telemetry.addData("Arm Position Motor1/Motor2", "%4.3f, %4.3f", pivotOne.getCurrentPosition(), pivotTwo.getCurrentPosition());
+            telemetry.addData("DT Power Levels FL/FR", "%4.2f, %4.2F", leftFrontDrive.getPower(), rightFrontDrive.getPower());
+            telemetry.addData("DT Power Levels BL/BR","%4.2f, %4.2f", leftBackDrive.getPower(),rightBackDrive.getPower());
+            //telemetry.addData("Encoder Pos Front : Left/Right", "%4.3f, %4.3f", flEncoder, frEncoder);
+            //telemetry.addData("Encoder Pos Back : Left/Right", "%4.3f, %4.3f", blEncoder, brEncoder);
+            telemetry.update();
+
+        }
+    }
+}
+
--- mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compModeOne.java
+++ mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compModeOne.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,45 +8,17 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.hardware.
 
-
-
-/*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name="Basic: compModeOne", group="Linear OpMode")
 //@Disabled
 public class compModeOne extends LinearOpMode {
-
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
@@ -59,37 +31,42 @@ public class compModeOne extends LinearOpMode {
     private DcMotor linSlideLeft = null;
     private DcMotor linSlideRight = null;
     private Servo intakeServo = null;
+    private IMU imu = null;
 
     @Override
     public void runOpMode() {
 
-        //Gamepad gamepad1 = new Gamepad();
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
+        //IMU variable / hardware map
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        //IMU parameters
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+        imu.initialize(parameters);
+
+        //Hardware map
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFrontDrive");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBackDrive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
-
         pivotOne = hardwareMap.get(DcMotor.class, "pivotOne");
         pivotTwo = hardwareMap.get(DcMotor.class, "pivotTwo");
-
         linSlideLeft = hardwareMap.get(DcMotor.class, "linSlideLeft");
         linSlideRight = hardwareMap.get(DcMotor.class, "linSlideRight");
-
         intakeServo = hardwareMap.get(Servo.class, "intakeServo");
 
+        //Motor Direction Set
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-
         pivotOne.setDirection(DcMotor.Direction.REVERSE);
-        pivotTwo.setDirection(DcMotor.Direction.REVERSE);
-
+        pivotTwo.setDirection(DcMotor.Direction.FORWARD);
         linSlideRight.setDirection(DcMotorSimple.Direction.FORWARD);
         linSlideLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        //Breaking modes for motors
         leftFrontDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
         leftBackDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
         rightFrontDrive.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
@@ -106,19 +83,20 @@ public class compModeOne extends LinearOpMode {
         telemetry.update();
 
 
+
         waitForStart();
         runtime.reset();
 
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double max;
+            //double max;
             double pivot;
             double slide;
             double intake;
 
-
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+//Robot Centric Control within comment bracket.
+            /*// POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             double axial   = gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
             double lateral =  -gamepad1.left_stick_x;
             double yaw     = -gamepad1.right_stick_x;//gamepad1.right_stick_x;
@@ -144,18 +122,75 @@ public class compModeOne extends LinearOpMode {
                 leftBackPower /= max;
                 rightBackPower /= max;
             }
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
             rightFrontDrive.setPower(rightFrontPower);
             leftBackDrive.setPower(leftBackPower);
             rightBackDrive.setPower(rightBackPower);
+*/
+
+            //IMU direction reset button
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = -gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            leftFrontDrive.setPower(frontLeftPower);
+            leftBackDrive.setPower(backLeftPower);
+            rightFrontDrive.setPower(frontRightPower);
+            rightBackDrive.setPower(backRightPower);
 
             //pivot logic
             if(gamepad1.right_trigger > 0.3){
-                pivot = 1;
+                pivot = gamepad1.right_trigger;
             }else if(gamepad1.left_trigger > 0.3) {
-                pivot = -1;
+                pivot = -(gamepad1.left_trigger);
             }else{
                 pivot = 0;
             }
@@ -171,9 +206,9 @@ public class compModeOne extends LinearOpMode {
 
             //intake logic
             if(gamepad1.a){
-                intake = 45;
+                intake = 0.75;
             }else{
-                intake = 0;
+                intake = 0.25;
             }
 
             //Set Power To motors as well as reset defaults
@@ -185,11 +220,12 @@ public class compModeOne extends LinearOpMode {
 
             //Show status, DT wheel power, and slide / intake debug
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
             telemetry.addData("Intake Position", "%3f", intakeServo.getPosition());
             telemetry.update();
 
         }
-    }}
+    }
+}
 
