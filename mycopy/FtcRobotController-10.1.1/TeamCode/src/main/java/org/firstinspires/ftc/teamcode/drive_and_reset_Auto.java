133c081946a1433797be3f100780341e413dc194
Jammsy <90413617+Jammsy@users.noreply.github.com>
2025-01-16 12:26:01 -0600

    THIS IS EVERYTHING

--- mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive_and_reset_Auto.java
+++ mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive_and_reset_Auto.java
@@ -1,21 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.teamcode.external_methods.*;
+import static java.lang.Thread.sleep;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-//import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp(name = "compModeTwo_iterative", group = "Iterative OpMode")
-public class compModeTwo_Iterative extends OpMode{
+@Autonomous
+public class drive_and_reset_Auto extends OpMode{
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
@@ -26,24 +26,25 @@ public class compModeTwo_Iterative extends OpMode{
     private DcMotor linSlideLeft = null;
     private DcMotor linSlideRight = null;
     private Servo intakeServo = null;
-   // private IMU imu = null;
+    private IMU imu = null;
     private double slideMax = -5000;
-    private int ZERO= 0, LOW_RUNG= 400, HIGH_RUNG= 600,LOW_BASET = 640, GROUND = 55, SUB = 150;
+    private int ZERO= 0, HIGH_RUNG= 600;//LOW_RUNG= 400, HIGH_RUNG= 600,LOW_BASET = 640, GROUND = 55, SUB = 150;
+    private enum pivotStates {START, RAISE, SLIDE, INTAKE_CLOSE, INTAKE_OPEN, DRIVE};
+    private pivotStates pivotState = pivotStates.START;
     private int pivotPose = 0;
-    //private SparkFunOTOS imu;
-    //private double e = 0.0;
+
+
 
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        /*imu = hardwareMap.get(IMU.class, "imu");
+        imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
-        imu.initialize(parameters);*/
+        imu.initialize(parameters);
 
-        //imu = hardwareMap.get(SparkFunOTOS.class, "imu");
         leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
         leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
@@ -63,15 +64,22 @@ public class compModeTwo_Iterative extends OpMode{
         pivotOne.setDirection(DcMotorSimple.Direction.FORWARD);
         pivotTwo.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         pivotOne.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         pivotTwo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        intakeServo.setDirection(Servo.Direction.FORWARD);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         pivotOne.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         pivotTwo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        linSlideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        linSlideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        linSlideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linSlideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -79,72 +87,48 @@ public class compModeTwo_Iterative extends OpMode{
 
     @Override
     public void start() {
-        reset_runWithoutEncoder(linSlideLeft, linSlideRight);
-        reset_runWithEncoder(pivotOne, pivotTwo);
+        resetSlideEncoders();
         runtime.reset();
-        //resetYaw();
+        resetYaw();
     }
+
     @Override
     public void loop() {
-        boolean lowRungButton = gamepad1.dpad_up;
-        boolean highRungButton = gamepad1.dpad_right;
-        boolean submersibleButton = gamepad1.dpad_down;
-        boolean slideOutTrigger = gamepad1.right_bumper;
-        boolean slideInTrigger = gamepad1.left_bumper;
-        boolean intakeCloseButton = gamepad1.triangle;
-        boolean pivotZeroButton = gamepad1.circle;
-        //boolean intakeOpenButton = gamepad1.cross;
+        linSlideLeft.setPower(0.8);
+        linSlideRight.setPower(0.8);
+        drivetrain(leftFrontDrive.getCurrentPosition());
+        pivotRun(pivotPose);
+        telemetry.addData("Left Front Drive encoder pos", "%s %s", leftFrontDrive.getCurrentPosition() );
+        telemetry.addData("Left Back Drive Encoder pos", "%s %s", leftBackDrive.getCurrentPosition());
+        telemetry.addData("Right Front Drive encoder pos", "%s %s", rightFrontDrive.getCurrentPosition());
+        telemetry.addData("Right Back Drive Encoder Pos" , "%s %s", rightBackDrive.getCurrentPosition());
 
-        //double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        switch(pivotState){
 
-        drivetrain(-gamepad1.left_stick_y, gamepad1.left_stick_x*1.1,-gamepad1.right_stick_x, leftFrontDrive, leftBackDrive,rightFrontDrive,rightBackDrive);
-        //drivetrain(-gamepad1.left_stick_y, gamepad1.left_stick_x*1.1,-gamepad1.right_stick_x,bot_heading,leftFrontDrive, leftBackDrive,rightFrontDrive,rightBackDrive);
+            case START :
+                intakeClose();
+                pivotState = pivotStates.RAISE;
 
-        if (lowRungButton) {
-            pivotPose = LOW_RUNG;
-        }else if(highRungButton){
-            pivotPose = HIGH_RUNG;
-        }else if(submersibleButton) {
-            pivotPose = SUB;
-        }else if(pivotZeroButton){
-            pivotPose = ZERO;
-        }else{
-            pivotPose = GROUND;
+                break;
+            case DRIVE:
+
+
+
+            case RAISE :
+                pivotPose = HIGH_RUNG;
+                linSlideLeft.setTargetPosition(-3500);
+                linSlideRight.setTargetPosition(-3500);
+                pivotState = pivotStates.SLIDE;
+                break;
+            case SLIDE :
+                linSlideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                linSlideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
         }
 
-        if((linSlideLeft.getCurrentPosition() > -5000) && slideOutTrigger){
-            linSlideLeft.setPower(-0.75);
-            linSlideRight.setPower(-0.75);
-            //slideOut(linSlideLeft.getCurrentPosition(), slideMax);
-        }else if(slideInTrigger && (linSlideLeft.getCurrentPosition() <= -2)){
-            linSlideLeft.setPower(0.75);
-            linSlideRight.setPower(0.75);
-            //slideIn(linSlideLeft.getCurrentPosition());
-        }else {
-            linSlideLeft.setPower(0);
-            linSlideRight.setPower(0);
-        }
-
-        if(intakeCloseButton){
-            //e+=0.01;
-            intakeClose(intakeServo);
-        }else{
-            intakeOpen(intakeServo);
-            //intakeClose(e);
-        }
-
-        if (gamepad1.options) {
-            //resetYaw();
-        }else if(gamepad1.touchpad){
-            reset_runWithoutEncoder(linSlideLeft, linSlideRight);
-        }else if(gamepad1.share){
-            reset_runWithEncoder(pivotOne, pivotTwo);
-        }
-
-        pivotRun(pivotPose, pivotOne, pivotTwo);
-
+        pivotRun(pivotPose);
         telemetry.addData("Pivot Encoder Pos", "%s, %s", pivotOne.getCurrentPosition(), pivotTwo.getCurrentPosition());
-        telemetry.addData("Intake Pos", "%s",intakeServo.getPosition());
         telemetry.addData("Lin Slide Encoder L | R", "%s, %s", linSlideLeft.getCurrentPosition(), linSlideRight.getCurrentPosition());
         telemetry.update();
     }
@@ -166,6 +150,10 @@ public class compModeTwo_Iterative extends OpMode{
         rightFrontDrive.setPower(frontRightPower);
         rightBackDrive.setPower(backRightPower);
     }*/
+    public void drivetrain(int current_pos){
+
+    }
+
     @Override
     public void stop() {
         //Good practice to stop motors in stop
@@ -177,5 +165,59 @@ public class compModeTwo_Iterative extends OpMode{
         rightBackDrive.setPower(0);
         pivotOne.setPower(0);
         pivotTwo.setPower(0);
+    }
+
+    public void resetSlideEncoders() {
+        linSlideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linSlideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linSlideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Usually you want to use encoders
+        linSlideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Unless you are using a limit switch
+    }
+
+    public void resetYaw() {
+        imu.resetYaw();
+    }
+
+    public void intakeOpen() {
+        intakeServo.setPosition(0.35);
+    }
+
+    public void intakeClose() {
+        intakeServo.setPosition(0.65);
+    }
+    public void pivotRun(int e){
+        if(e != 0) {
+            pivotOne.setPower(0.6);
+            pivotTwo.setPower(0.6);
+            pivotOne.setTargetPosition(e);
+            pivotTwo.setTargetPosition(e);
+            pivotOne.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            pivotTwo.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }else{
+            pivotOne.setPower(0);
+            pivotTwo.setPower(0);
+            pivotOne.setTargetPosition(e);
+            pivotTwo.setTargetPosition(e);
+        }
+    }
+
+    public void slideOut(int pos, double max) {
+        if (pos < max) {
+            linSlideLeft.setPower(0.6);
+            linSlideRight.setPower(0.6);
+        } else {
+            linSlideLeft.setPower(0);
+            linSlideRight.setPower(0);
+        }
+    }
+
+    public void slideIn(int slidePos) {
+        if (slidePos > 0) {
+            linSlideLeft.setPower(-0.6); // Made negative to retract
+            linSlideRight.setPower(-0.6);
+        } else {
+            linSlideLeft.setPower(0);
+            linSlideRight.setPower(0);
+        }
     }
 }
---
+++ mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/external_methods.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+public class external_methods  extends compModeTwo_Iterative{
+    public static void drivetrain(double y,double x, double rx, DcMotor one, DcMotor two,DcMotor three, DcMotor four){
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        one.setPower(frontLeftPower);
+        two.setPower(backLeftPower);
+        three.setPower(frontRightPower);
+        four.setPower(backRightPower);
+    }
+
+    public static void drivetrain(double y,double x, double rx, double heading, DcMotor one, DcMotor two,DcMotor three, DcMotor four){
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        one.setPower(frontLeftPower);
+        two.setPower(backLeftPower);
+        three.setPower(frontRightPower);
+        four.setPower(backRightPower);
+    }
+
+    public static void reset_runWithoutEncoder(DcMotor one, DcMotor two) {
+        one.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        two.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        one.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Usually you want to use encoders
+        two.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Unless you are using a limit switch
+    }
+    public static void reset_runWithEncoder(DcMotor one, DcMotor two){
+        one.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        two.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        one.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        two.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public static void intakeOpen(Servo s) {
+        //0.65
+        s.setPosition(0.5);
+
+    }
+
+    public static void intakeClose(Servo s) {
+        //0.75
+        s.setPosition(0.8);
+    }
+    public static void pivotRun(int e, DcMotor one, DcMotor two){
+        if(e != 0) {
+            one.setPower(0.6);
+            two.setPower(0.6);
+            one.setTargetPosition(e);
+            two.setTargetPosition(e);
+            one.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            two.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }else{
+            one.setPower(0);
+            two.setPower(0);
+            one.setTargetPosition(e);
+            two.setTargetPosition(e);
+        }
+    }
+    public static void slideOut(int pos, double max, DcMotor one, DcMotor two) {
+        if (pos < max) {
+            one.setPower(0.6);
+            two.setPower(0.6);
+        } else {
+            one.setPower(0);
+            two.setPower(0);
+        }
+    }
+
+    public static void slideIn(int slidePos, DcMotor one, DcMotor two) {
+        if (slidePos < 0) {
+            one.setPower(-0.6); // Made negative to retract
+            two.setPower(-0.6);
+        } else {
+            one.setPower(0);
+            two.setPower(0);
+        }
+    }
+
+    public static void configureIMU(SparkFunOTOS x){
+        x.setLinearUnit(DistanceUnit.INCH);
+        x.setAngularUnit(AngleUnit.DEGREES);
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        x.setOffset(offset);
+    }
+}
